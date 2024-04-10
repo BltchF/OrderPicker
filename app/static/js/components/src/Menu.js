5cce128d8ea535a26591305d0486558657460bfd
BltchF <military13215@gmail.com>
2024-04-10 19:37:57 +0800

    Temp Addon info adjust finish. - start to adjust modal into AddonPopup.js

--- app/static/js/components/src/Menu.js
+++ app/static/js/components/src/Menu.js
@@ -3,6 +3,8 @@ import QuantitySelector from './QuantitySelector';
 import AddonPopup from './AddonPopup';
 import "./Menu.css";
 import Modal from 'react-modal';
+import TempStoreAddon from './TempStoredAddon';
+import axios from 'axios';  
 
 function Menu({ menu, store_id }) {  
     const [showPopup, setShowPopup] = React.useState(false);
@@ -15,50 +17,56 @@ function Menu({ menu, store_id }) {
     };
 
     const orderItem = () => {
-        const selectedAddons = JSON.parse(localStorage.getItem('selectedAddons')) || [];
-
         const items = [{
             item_id: menu.item_id,
             quantity: quantity,
             additions: selectedAddons.map(addon => addon.id),
         }];
 
-        fetch('/api/order', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                store_id: store_id,
-                user_id: user_id,
-                items: items,
-            }),
+        axios.post('/api/order', {
+            store_id: store_id,
+            user_id: user_id,
+            items: items,
+        })
+        .then(response => {
+            console.log(response); // Log the response
+        })
+        .catch(error => {
+            console.log(error); // Log any errors
         });
 
+        // Update the selectedAddons state
+        setSelectedAddons([]);
+        // Remove selectedAddons from localStorage
         localStorage.removeItem('selectedAddons');
     };
 
     return (
-        <div className="flex flex-row py-1 min-w-200">
-            <div className="w-1/3">
-                {menu.item_name}
+        <div className="flex flex-col">
+            <div className="flex flex-row py-1 min-w-200">
+                <div className="w-1/3">
+                    {menu.item_name}
+                </div>
+                <div className="w-2/3 flex flex-nowrap justify-between">
+                    <button className="rounded-md bg-blue-500 py-1 px-1 flex-nowrap" onClick={() => setShowPopup(true)}>調整</button>
+                    <QuantitySelector item_id={menu.item_id}
+                        quantity={quantity}
+                        setQuantity={setQuantity} />
+                    <button className="rounded-md bg-blue-500 py-1 px-1 flex-nowrap" onClick={orderItem}>買這個</button>
+                    <Modal
+                        isOpen={showPopup}
+                        onRequestClose={() => setShowPopup(false)}
+                        contentLabel="Addon Popup"
+                    >
+                        <AddonPopup item_id={menu.item_id} onAddAddon={handleAddAddon} onClose={() => setShowPopup(false)} />
+                    </Modal>
+                </div>
             </div>
-            <div className="w-2/3 flex flex-nowrap justify-between">
-                <button className="rounded-md bg-blue-500 py-1 px-1 flex-nowrap" onClick={() => setShowPopup(true)}>調整</button>
-                <QuantitySelector item_id={menu.item_id} 
-                                quantity={quantity}
-                                setQuantity={setQuantity}/>
-                <button className="rounded-md bg-blue-500 py-1 px-1 flex-nowrap" onClick={orderItem}>買這個</button>
-                <Modal 
-                    isOpen={showPopup}
-                    onRequestClose={() => setShowPopup(false)}
-                    contentLabel="Addon Popup"
-                >
-                    <AddonPopup item_id={menu.item_id} onAddAddon={handleAddAddon} onClose={() => setShowPopup(false)} />
-                </Modal>
+            <div>
+                <TempStoreAddon selectedAddons={selectedAddons} />
             </div>
         </div>
     );
-}
+};
 
 export default Menu;
---
+++ app/static/js/components/src/TempStoredAddon.js
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const addonStyle = css`
+    border: 1px solid black;
+    margin: 10px;
+    padding: 10px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`;
+
+function TempStoreAddon({ selectedAddons }) {
+    console.log(selectedAddons); // debug information, should be removed later
+
+    const flatSelectedAddons = selectedAddons.flat();
+
+
+    return (
+        <div>
+            {flatSelectedAddons.map((addon) => (
+                <div key={addon.id} css={addonStyle}>
+                    <span>[+]:{addon.add_name}</span>
+                    <span>/$: {addon.add_price}</span>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default TempStoreAddon;
